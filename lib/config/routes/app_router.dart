d54c9f557c2ec04e16b5dc41c78a545627420e24
samu7861 <sfrancog7861@gmail.com>
2024-11-02 22:05:43 -0500

    app router

---
+++ lib/config/routes/app_router.dart
@@ -0,0 +1,11 @@
+import 'package:auto_route/auto_route.dart';
+
+part 'app_router.gr.dart';
+
+@AutoRouterConfig()
+class AppRouter extends _$AppRouter {
+  @override
+  // TODO: implement routes
+  List<AutoRoute> get routes => [];
+
+}
